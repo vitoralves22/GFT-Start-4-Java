8395a7777301dd25eeb3753129a9cd894c1a3c9b
Vitor Alves <vitornobody@gmail.com>
2022-02-15 18:28:43 -0300

    using dependency injection

--- SpringBasicConcepts/src/main/java/com/example/injecaodedependencia/Carro.java
+++ SpringBasicConcepts/src/main/java/com/example/injecaodedependencia/Carro.java
@@ -1,4 +1,4 @@
-package com.example.di;
+package com.example.injecaodedependencia;
 
 public class Carro implements Veiculo {
 	
--- SpringBasicConcepts/src/main/java/com/example/injecaodedependencia/Moto.java
+++ SpringBasicConcepts/src/main/java/com/example/injecaodedependencia/Moto.java
@@ -1,4 +1,4 @@
-package com.example.di;
+package com.example.injecaodedependencia;
 
 public class Moto implements Veiculo {
 	@Override
